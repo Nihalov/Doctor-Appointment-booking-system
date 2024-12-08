5d85209df6bf78375b807dcb2bec6765c5731113
Nihal_ov <nihalov89@gmail.com>
2024-12-08 23:56:02 +0530

    database setup:

---
+++ d_a_b.py
@@ -0,0 +1,12 @@
+import psycopg2
+import json
+# Establish the connection
+def connect_to_db():
+    return psycopg2.connect(
+        dbname="doctorAppointmentDb",
+        user="postgres",
+        password="Superuser@123",
+        host="localhost",
+        port= "5432"
+    )
+    
